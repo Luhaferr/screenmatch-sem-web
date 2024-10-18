02ac3a45ecda1a621f7565b5299a5bf63fa90f7e
Luiz Ferreira <luhaferr@gmail.com>
2024-10-18 16:13:23 -0300

    novas classes para dados

--- src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
+++ src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatchspring;
 
+import br.com.alura.screenmatchspring.model.DadosEpisodio;
 import br.com.alura.screenmatchspring.model.DadosSerie;
+import br.com.alura.screenmatchspring.model.DadosTemporada;
 import br.com.alura.screenmatchspring.service.ConsumoApi;
 import br.com.alura.screenmatchspring.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchspringApplication implements CommandLineRunner {
@@ -17,10 +22,24 @@ public class ScreenmatchspringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=ec5e903f");
+		String json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=ec5e903f");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
+		//a classe já é instanciada recebendo os dados json convertidos em objeto para preencher as informações da classe.
 		DadosSerie dados = conversor.converteDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=ec5e903f");
+		DadosEpisodio dadosEpisodio = conversor.converteDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i = 1; i < dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=ec5e903f");
+			DadosTemporada dadosTemporada = conversor.converteDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		//escreve as temporadas
+		temporadas.forEach(System.out::println);
 	}
 }
